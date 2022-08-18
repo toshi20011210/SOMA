605edf128877b2fc3693d394f964e94d59dc6002
Mac Pro Mid 2010 <japan.toshi.taka@gmail.com>
2022-08-18 21:11:47 +0900

    git push

---
+++ tenchi.py
@@ -0,0 +1,17 @@
+from PIL import Image
+from PIL import ImageOps
+
+# 画像を開く
+img = Image.open("6.png")
+
+# 画像を回転する
+img_rotate = img.rotate(90)
+
+# 回転した画像の保存
+img_rotate.save("rotated.png")
+
+timg = Image.open("rotated.png")
+
+img_mirror = ImageOps.mirror(timg)
+
+img_mirror.save("replace.png")
